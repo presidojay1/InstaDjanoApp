2e3bf3db3191a4fd2c0539d088dca7fa460cc8a0
abayomicornelius <ayoabayomi04@gmail.com>
2024-08-21 13:07:25 +0100

    testpushh

--- InstagramDjangoApp/tasks.py
+++ InstagramDjangoApp/tasks.py
@@ -1,9 +1,11 @@
+
 from celery import shared_task
 from django.utils import timezone
 from .models import Profile, InstagramAccount
 from InstagramDjangoApp.bot import Bot
 import datetime
 from datetime import timedelta
+
 
 
 @shared_task
@@ -17,28 +19,38 @@ def perform_instagram_tasks():
     # Filter profiles with a subscription end date less than a month from now
     profiles = Profile.objects.filter(subscription_end_date__lt=one_month_from_now)
 
-    # Check if any profiles were retrieved
-    if profiles.exists():
-        print(f"Found {profiles.count()} profiles with active subscriptions.")
-    else:
-        print("No profiles with active subscriptions found.")
-
+    
     for profile in profiles:
-        # Get Instagram accounts associated with the profile
-        accounts = InstagramAccount.objects.filter(profile=profile)
-
-        # Check if any Instagram accounts were retrieved
-        if accounts.exists():
-            print(f"Found {accounts.count()} Instagram accounts for profile {profile.username}.")
+        # Check subscription plan and perform actions accordingly
+        if profile.subscription_plan == 'basic':
+            # Perform bot actions for basic plan
+            perform_bot_actions_for_profile(profile)
+        elif profile.subscription_plan in ['medium', 'premium']:
+            # Perform bot actions for medium and premium plans (e.g., more frequent tasks)
+            perform_bot_actions_for_profile(profile)
         else:
-            print(f"No Instagram accounts found for profile {profile.username}.")
+            # Handle other cases, if needed
+            pass
 
+def perform_bot_actions_for_profile(profile):
+    # Ensure the profile has Instagram accounts
+    accounts = InstagramAccount.objects.filter(profile=profile)
+    if accounts.exists():
         for account in accounts:
+            # Initialize the bot with account credentials
+            print(username=account.username, password=account.password)
             bot = Bot(username=account.username, password=account.password)
+            
             try:
+                # Perform bot actions
                 bot.like_stories()
                 bot.like_posts_from_feed()
+      
             except Exception as e:
                 print(f"An error occurred while performing actions for account {account.username}: {e}")
+            
             finally:
+                # Close browser after operations
                 bot.browser.quit()
+    else:
+        print(f"No Instagram accounts found for profile {profile.username}.")
